ca954b2936ab7b01903a44f174b397d66b6f725d
Mendel Kahan <mendy@famkahan.com>
2020-01-24 15:59:30 +0000

    j

---
+++ app/Http/Controllers/Api/V1/PermissionController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Permission;
+use App\Http\Resources\Permission as PermissionResource;
+use App\Http\Requests\StorePermission;
+
+class PermissionController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       return PermissionResource::collection(Permission::paginate(10)); 
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePermission $request)
+    {
+        // isset($this->item->id) ? $itemID = $this->item->id : $itemID = "";
+        
+        $validatedData = $request->validated();
+        $permission = new Permission();
+        $new_permission = $role-> create($validatedData);
+        return new PermissionResource( $new_permission );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Permission $permission)
+    {
+        return new PermissionResource($permission);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+     public function update(StorePermission $request, $id)
+    {
+        $validatedData = $request->validated();
+         $result = Permission::where('id', '=', $id)->update($validatedData);
+         if($result){
+             return response()->json(['success' => 'true']);
+         }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
+
+}
